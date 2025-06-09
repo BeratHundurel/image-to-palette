092673e19c5935b93a812c0fc488f1ae6a2aeff9
Berat Hündürel <berat.hundurel@bilgiedu.net>
2025-06-09 21:32:45 +0300

    Start draggable toolbar component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Draggable from '$lib/components/Draggable.svelte';
 	import Dropdown from '$lib/components/Dropdown.svelte';
 	import { type Color, type NamedColor, type PaletteResponse } from '$lib/types/palette';
 	import { tick } from 'svelte';
@@ -358,7 +359,7 @@
 
 	<!-- Content -->
 	<div
-		class="relative z-20 flex min-h-[100svh] w-full flex-col items-center justify-center overflow-hidden px-4"
+		class="relative z-20 flex min-h-[100svh] w-full flex-col items-center justify-center overflow-hidden"
 	>
 		<h1
 			class="absolute top-[25%] right-0 left-0 text-center text-3xl font-bold tracking-tight drop-shadow-lg transition-transform duration-500 md:text-4xl"
@@ -405,62 +406,69 @@
 			</button>
 		</div>
 
-		<div class="flex flex-row items-start justify-center gap-3">
-			<canvas
-				bind:this={canvas}
-				onmousedown={handleMouseDown}
-				onmousemove={handleMouseMove}
-				onmouseup={handleMouseUp}
-				class="mb-6 h-auto max-h-[400px] w-full max-w-3xl rounded-xl shadow-lg transition-opacity duration-300"
-				class:opacity-100={imageLoaded}
-				class:pointer-events-auto={imageLoaded}
-				class:opacity-0={!imageLoaded}
-				class:pointer-events-none={!imageLoaded}
-			></canvas>
-			{#if imageLoaded}
-				<div class="flex flex-row gap-1">
-					<div
-						transition:fly={{ y: -300, duration: 500 }}
-						class="flex flex-col items-start justify-center gap-3"
-					>
-						{#each selectors as selector}
-							<button
-								class="flex cursor-pointer flex-col items-center justify-center"
-								onclick={() => {
-									activeSelectorId = selector.id;
-									selectors = selectors.map((s) =>
-										s.id === selector.id ? { ...s, selected: true } : { ...s, selected: false }
-									);
-								}}
-							>
-								<div
-									class="flex h-8 w-8 items-center justify-center rounded-full shadow-md"
-									style="background-color: {selector.color}"
+		<div class="mb-6 grid grid-cols-4">
+			<div></div>
+			<div class="col-span-2 me-6">
+				<canvas
+					bind:this={canvas}
+					onmousedown={handleMouseDown}
+					onmousemove={handleMouseMove}
+					onmouseup={handleMouseUp}
+					class="h-auto max-h-[400px] w-full max-w-3xl rounded-xl shadow-lg transition-opacity duration-300"
+					class:opacity-100={imageLoaded}
+					class:pointer-events-auto={imageLoaded}
+					class:opacity-0={!imageLoaded}
+					class:pointer-events-none={!imageLoaded}
+				></canvas>
+			</div>
+			<div>
+				{#if imageLoaded}
+					<div class="flex flex-col items-start gap-4">
+						<div class="flex flex-col gap-2">
+							{#each selectors as selector}
+								<button
+									class="flex cursor-pointer flex-col items-center justify-center"
+									onclick={() => {
+										activeSelectorId = selector.id;
+										selectors = selectors.map((s) =>
+											s.id === selector.id ? { ...s, selected: true } : { ...s, selected: false }
+										);
+									}}
 								>
-									{#if selector.selected}
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											height="20px"
-											viewBox="0 -960 960 960"
-											width="20px"
-											fill="#000"
-											><path
-												d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
-											/></svg
-										>
-									{/if}
-								</div>
-							</button>
-						{/each}
+									<div
+										class="flex h-8 w-8 items-center justify-center rounded-full border-black shadow-md"
+										style="background-color: {selector.color}"
+										class:border-2={selector.selected}
+									>
+										{#if selector.selected}
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												height="20px"
+												viewBox="0 -960 960 960"
+												width="20px"
+												fill="#000"
+												><path
+													d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+												/></svg
+											>
+										{/if}
+									</div>
+								</button>
+							{/each}
+						</div>
+
+						<!-- dropdown for drawing options -->
 						<Dropdown
 							options={draw_options}
 							value={drawSelectionValue}
 							onChange={handleDrawSelectionChange}
 						/>
 					</div>
-				</div>
-			{/if}
+				{/if}
+			</div>
 		</div>
+
+		<Draggable />
 
 		<div class="w-full max-w-5xl px-4">
 			<div
